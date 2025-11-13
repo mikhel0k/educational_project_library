11b9f343b9455019f630bb59e1b81ed08fb9f003
Mikhel Lev <levmihel150@gmail.com>
2025-11-13 21:21:31 +0700

    Create average rating gield, but comment it

--- core/database/models/Book.py
+++ core/database/models/Book.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Text, Float, Integer
+from sqlalchemy import String, Text, Float, Integer, Computed
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .Base import BaseModel
@@ -10,7 +10,11 @@ class Book(BaseModel):
     publication_year: Mapped[int] = mapped_column(Integer, nullable=False)
     genre: Mapped[str] = mapped_column(String(128), nullable=False)
     language: Mapped[str] = mapped_column(String(128), nullable=False)
-    average_rating: Mapped[float] = mapped_column(Float, default=0.0)
+    # average_rating: Mapped[float] = mapped_column(
+    #     Computed("COALESCE((SELECT AVG(rating) FROM reviews WHERE reviews.book_id = id), 0.0)",
+    #              persisted=True,
+    #              )
+    # )
 
     physical_books: Mapped[list["PhysicalBook"]] = relationship("PhysicalBook", back_populates="book")
     ebook: Mapped["Ebook | None"] = relationship("Ebook", back_populates="book", uselist=False)
