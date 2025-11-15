e6300ea369d79b013e4d1bdcce6016168ebb3f1c
Mikhel Lev <levmihel150@gmail.com>
2025-11-15 10:07:02 +0700

    Reinicialize project because I want to change smthng

--- core/database/db.py
+++ core/database/db.py
@@ -1,22 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from ..settings import get_db_url
+from sqlalchemy.ext.asyncio import create_async_engine
 
 
-ASYNC_DATABASE_URL = get_db_url()
-
-async_engine = create_async_engine(ASYNC_DATABASE_URL)
-
-AsyncSessionLocal = async_sessionmaker(
-    async_engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-    autoflush=False,
-)
-
-
-async def get_db():
-    async with AsyncSessionLocal() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
+async_engine = create_async_engine()
--- core/settings.py
+++ core/settings.py
@@ -1,21 +1,21 @@
 import os
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_NAME: str
-    DB_USER: str
-    DB_PASSWORD: str
-    model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
-    )
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: int = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
 
 
 settings = Settings()
 
 
 def get_db_url():
-    return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
+    return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"  
             f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
