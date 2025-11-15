5c0bf093de4c74255788770dce16c4c998989bf1
Mikhel Lev <levmihel150@gmail.com>
2025-11-15 11:09:55 +0700

    Create db engine and session fabric

--- core/database/db.py
+++ core/database/db.py
@@ -1,5 +1,20 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from ..settings import get_db_url
 
 
-async_engine = create_async_engine()
+async_engine = create_async_engine(get_db_url())
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False
+)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
