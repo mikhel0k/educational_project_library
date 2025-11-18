dab791418ab40619f4ef30ef122c36d0d7adde39
Mikhel Lev <levmihel150@gmail.com>
2025-11-18 21:36:04 +0700

    Fix bugs

--- core/api/v1/Book.py
+++ core/api/v1/Book.py
@@ -14,7 +14,7 @@ router = APIRouter(prefix='/book', tags=['books'])
 
 
 @router.get('/get_by_title/{title}', response_model=list[BookResponse])
-async def get_book(
+async def get_book_by_title(
         title: str,
         session: AsyncSession = Depends(get_db)
 ):
@@ -22,7 +22,7 @@ async def get_book(
 
 
 @router.get('/get_by_isbn/{isbn}', response_model=list[BookResponse])
-async def get_book(
+async def get_book_by_isbn(
         isbn: str,
         session: AsyncSession = Depends(get_db)
 ):
