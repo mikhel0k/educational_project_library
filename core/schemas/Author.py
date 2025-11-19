959a69f7b82e04097c1fe7568189fb8a63283c6e
Mikhel Lev <levmihel150@gmail.com>
2025-11-19 12:22:27 +0700

    Create Authors schemas

---
+++ core/schemas/Author.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field, ConfigDict
+
+
+class BaseSchema(BaseModel):
+    first_name: str = Field(..., description="First Name")
+    second_name: str | None = Field(..., description="Second Name")
+    birth_date: datetime = Field(..., description="Birthday date")
+    bio: str | None = Field(..., description="Bio of author")
+
+
+class AuthorCreate(BaseSchema):
+    pass
+
+
+class AuthorUpdate(BaseModel):
+    first_name: str | None = Field(..., description="First Name")
+    second_name: str | None = Field(..., description="Second Name")
+    birth_date: datetime | None = Field(..., description="Birthday date")
+    bio: str | None = Field(..., description="Bio of author")
+
+
+class AuthorResponse(BaseSchema):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
+    updated_at: datetime
